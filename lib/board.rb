c0e92a841a6672848a2171694770af6a253a4cd1
otherdcn <danny.c.ntwari@gmail.com>
2024-10-28 17:12:23 +0200

    Add object instantiation defaults for Board class

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,27 @@
 class Board
   attr_reader :grid, :key
+
+  def initialize
+    @grid = Array.new(3) { Array.new(3, ".") }
+    @key = set_key
+  end
+
+  def set_key
+    column_ids = "A".upto("H").map(&:to_s)
+    row_ids = "1".upto("8").map(&:to_s)
+    final_board = []
+
+    row_ids.map do |rank|
+      row = []
+      column_ids.map do |file|
+        row << file+rank
+      end
+      final_board << row
+      row = []
+    end
+
+    final_board
+  end
+
+  private :set_key
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -4,5 +4,43 @@ describe Board do
   describe "attributes" do
     it { expect(subject).to respond_to :grid }
     it { expect(subject).to respond_to :key }
+
+    context "given the grid attribute" do
+      before do
+        @grid = subject.grid
+      end
+
+      it "is a multi-dimensional array" do
+        grid_first_element = @grid.first
+        grid_same_type = @grid.class == grid_first_element.class
+
+        expect(grid_same_type).to eq true
+      end
+
+      it "has a default value of '.' for every element" do
+        expect(@grid.flatten).to all( be_a(String).and include('.') )
+      end
+    end
+
+    context "given the key attribute" do
+      before do
+        @key = subject.key
+      end
+
+      it "is a multi-dimensional array" do
+        key_first_element = @key.first
+        key_same_type = @key.class == key_first_element.class
+
+        expect(key_same_type).to eq true
+      end
+
+      it "has the value of 'A1' as the first element " do
+        expect(@key.flatten.first).to eq 'A1'
+      end
+
+      it "has the value of 'H8' as the last element " do
+        expect(@key.flatten.last).to eq 'H8'
+      end
+    end
   end
 end
