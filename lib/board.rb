4f43d9d8c13f820d2818da259f1d81b1d7c338dc
otherdcn <danny.c.ntwari@gmail.com>
2024-10-29 21:12:23 +0200

    Add Board#insert_disk method
    Ensure input argument is valid and available before proceeding.

--- lib/board.rb
+++ lib/board.rb
@@ -4,6 +4,7 @@ class Board
   def initialize
     @grid = Array.new(6) { Array.new(7, ".") }
     @key = set_key
+    @points_marked = []
   end
 
   def set_key
@@ -21,6 +22,19 @@ class Board
     end
 
     final_board
+  end
+
+  def insert_disk(player_disk, point_id)
+    raise StandardError, "Invalid Point ID provided" unless valid?(point_id)
+    raise StandardError, "Unavailable Point ID provided" if available?(point_id)
+  end
+
+  def valid?(point_id)
+    key.flatten.include?(point_id)
+  end
+
+  def available?(point_id)
+    @points_marked.include?(point_id)
   end
 
   private :set_key
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -43,4 +43,94 @@ describe Board do
       end
     end
   end
+
+  describe "#insert_disk" do
+    subject(:board) { described_class.new }
+    let(:player_disk) { "X" }
+
+    context "checking validity of input" do
+      let(:valid_point) { "E3" }
+      let(:invalid_point) { ["EE3", "P3", "PP", "A", "4", ""] }
+
+      context "given a valid point 'E3'" do
+        it "raises no error" do
+          expect { board.insert_disk(player_disk, valid_point) }.to_not raise_error
+        end
+      end
+
+      context "given an invalid point 'EE3'" do
+        it "raises a StandardError instance" do
+          expect { board.insert_disk(player_disk, invalid_point[0]) }.to raise_error(StandardError, "Invalid Point ID provided")
+        end
+      end
+
+      context "given an invalid point 'P3'" do
+        it "raises a StandardError instance" do
+          expect { board.insert_disk(player_disk, invalid_point[1]) }.to raise_error(StandardError, "Invalid Point ID provided")
+        end
+      end
+
+      context "given an invalid point 'PP'" do
+        it "raises a StandardError instance" do
+          expect { board.insert_disk(player_disk, invalid_point[2]) }.to raise_error(StandardError, "Invalid Point ID provided")
+        end
+      end
+
+      context "given an invalid point 'A'" do
+        it "raises a StandardError instance" do
+          expect { board.insert_disk(player_disk, invalid_point[3]) }.to raise_error(StandardError, "Invalid Point ID provided")
+        end
+      end
+
+      context "given an invalid point '4'" do
+        it "raises a StandardError instance" do
+          expect { board.insert_disk(player_disk, invalid_point[4]) }.to raise_error(StandardError, "Invalid Point ID provided")
+        end
+      end
+
+      context "given an invalid point '' (empty string)" do
+        it "raises a StandardError instance" do
+          expect { board.insert_disk(player_disk, invalid_point[5]) }.to raise_error(StandardError, "Invalid Point ID provided")
+        end
+      end
+    end
+
+    context "checking availability of input" do
+      let(:unavailable_points) { %w[A6 B6 B5 C6 D6 E6 E5] }
+
+      before do
+        board.instance_variable_set(:@points_marked, unavailable_points)
+      end
+
+      context "given an available input A3" do
+        it "raises no error" do
+          expect { board.insert_disk(player_disk, "A3") }.to_not raise_error
+        end
+      end
+
+      context "given an unavailable input A6" do
+        it "raises a StandardError instance" do
+          expect { board.insert_disk(player_disk, unavailable_points[0]) }.to raise_error(StandardError, "Unavailable Point ID provided")
+        end
+      end
+
+      context "given an available input B6" do
+        it "raises a StandardError instance" do
+          expect { board.insert_disk(player_disk, unavailable_points[1]) }.to raise_error(StandardError, "Unavailable Point ID provided")
+        end
+      end
+
+      context "given an available input B5" do
+        it "raises a StandardError instance" do
+          expect { board.insert_disk(player_disk, unavailable_points[2]) }.to raise_error(StandardError, "Unavailable Point ID provided")
+        end
+      end
+
+      context "given an available input E5" do
+        it "raises a StandardError instance" do
+          expect { board.insert_disk(player_disk, unavailable_points[6]) }.to raise_error(StandardError, "Unavailable Point ID provided")
+        end
+      end
+    end
+  end
 end
